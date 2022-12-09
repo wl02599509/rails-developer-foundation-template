11fc31a41c986e279574921ccb0d1bfb21c6e300
蒼時弦也 <elct9620@frost.tw>
2022-12-10 00:09:09 +0800

    Initialize Project

---
+++ features/support/database_rewinder.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+BeforeAll do |_scenario|
+  DatabaseRewinder.clean_all
+end
+
+After do |_scenario|
+  DatabaseRewinder.clean
+end
---
+++ features/support/env.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+if ENV.fetch('COVERAGE', false) || ENV.fetch('CI', false)
+  require 'simplecov'
+  require 'simplecov-cobertura'
+
+  SimpleCov.formatter = SimpleCov::Formatter::CoberturaFormatter if ENV.fetch('GITLAB_CI', false)
+
+  SimpleCov.start 'rails' do
+    add_filter 'vendor'
+  end
+end
+
+require 'cucumber/rails'
+
+ActionController::Base.allow_rescue = false
+ActiveSupport::TestCase.file_fixture_path = Rails.root.join('features/support/files')
+DatabaseRewinder.strategy = :transaction
+Cucumber::Rails::Database.javascript_strategy = :truncation
